12edd74fb6791fcc104aed59729ba05b6d0c7327
huseynmammadov000 <memmedovhuseyn667@gmail.com>
2024-09-02 19:23:02 +0400

    new commit

--- src/screens/detailsPage/DetailsPage.jsx
+++ src/screens/detailsPage/DetailsPage.jsx
@@ -3,11 +3,11 @@ import {View, Text} from 'react-native';
 import {StyledText, StyledView} from '@common/StyledComponents';
 
 const DetailsPage = ({route}) => {
-  const { itemDetails } = route.params;  // Veriyi route.params ile alıyoruz
+  const { itemDetails } = route.params; 
 
   return (
     <StyledView className="p-4">
-      <StyledText className="text-2xl font-bold mb-4">{itemDetails.name}</StyledText>
+      <StyledText className="text-3xl font-bold mb-4">{itemDetails.name}</StyledText>
       <StyledText className="text-lg mb-2">Email: {itemDetails.email}</StyledText>
       <StyledText className="text-lg">Company: {itemDetails.company.name}</StyledText>
     </StyledView>
